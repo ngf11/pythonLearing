598f8786cc83802beeb5b0c9e922bf369cdb6991
Nicolas Fuentes <nfuentes11@gmail.com>
2024-03-11 20:34:19 -0400

    Py day two

--- main.py
+++ main.py
@@ -14,4 +14,14 @@ print(type(9.8))
 print(type(3.14))
 print(type(4-4j))
 print(type(['Asabeneh', 'Python', 'Finland']))
+print("---------")
 print(type("nico"))
+print(type(11))
+print(type(9.8))
+print(type(2+1j)) #complex
+print(type(True))
+print(type(["nico","milo","didi","mama"])) #list
+print(type({"name":"fat"})) #dictonary
+print(type({"nico",369,"Uruguay"})) #set
+print(type((1,2,3,4))) #tuple
+
