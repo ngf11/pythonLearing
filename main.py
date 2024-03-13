a84724c244f9c0ae9a10ef7ff55bf388b3718bae
Nicolas Fuentes <nfuentes11@gmail.com>
2024-03-12 21:09:13 -0400

    Py day two

--- main.py
+++ main.py
@@ -24,4 +24,30 @@ print(type(["nico","milo","didi","mama"])) #list
 print(type({"name":"fat"})) #dictonary
 print(type({"nico",369,"Uruguay"})) #set
 print(type((1,2,3,4))) #tuple
+first_name='nico'
+last_name ="fuentes"
+country = 'United States'
+city ='kearny'
+age = 35
+is_married = True
+skills = ['HTML','CSS','JavaScript',"react","node"," Python","Blender"]
+person_info ={
+    "firstname":"nico",
+    "lastname": "fuentes",
+    "nationality": "Uruguay",
+    "city": "Montevide"
+}
+print(len(skills))
+print(len(person_info))
+# Printing the values stored in the variables
 
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
