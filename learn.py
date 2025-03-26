883e784a5513fea8fbd7b6b93996809a787f79e0
Nicolas Fuentes <nfuentes11@gmail.com>
2025-03-26 10:04:36 -0400

    Py

---
+++ learn.py
@@ -0,0 +1,50 @@
+# from ast import If
+# from distutils.command.build_scripts import first_line_re
+# from tkinter import Variable
+# print("Hello")
+# print("""Choad 
+# Fat 
+# Ugly""")
+# print("Hello\nWorld \nFatty")
+# print("Hello\nWorld \nFatty")
+# print("nico" * 10)
+# print("nico\n" * 10)
+
+# # Variable
+# name = "Choad"
+# print(name)
+# first_name = "Nicolas"
+# last_name = "Fuentes"
+# age = 35
+# print(f"Hello,\n My name {first_name} {last_name}. I am {age} years old ")
+# print("Hi, "+ input("What is your name?"))
+
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+
+
+# name = "Luis"
+# # age = 35
+# a = 1
+# b= 1
+# # c = "1"
+# d= True
+
+# print(name)
+
+# print(a + b)
+# # print(a+b+c)
+# print(d)
+
+# fruit = ["apple", "banana"]
+
+
--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const { PythonShell } = require("python-shell");
 PythonShell.runString("x=1+1;print(x)", null).then((messages) => {
   console.log("finished");
 });
+
+let name = "nico";
+
+if ((name.length = 4 | (name.length >= 4))) {
+  console.log("Gay");
+}
